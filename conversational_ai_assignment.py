e6333d2f400da2998c524fac4d3a706eb6b93e8e
abhinav078 <43104472+abhinav078@users.noreply.github.com>
2022-10-16 17:54:52 -0400

    Update conversational_ai_assignment.py

--- conversational_ai_assignment.py
+++ conversational_ai_assignment.py
@@ -1,5 +1,21 @@
-from flask import Flask
+from flask import Flask, request
+import json
+
+# initialize the flask app
 app = Flask(__name__)
-@app.route('/')
-def hello():
-    return 'Hello, World!'
+
+@app.route('/route')
+def route():
+  studentID = 200490537
+  value = {'StudentID' : studentID}
+  return json.dump(value)
+
+@app.route('/webhook', methods=['GET', 'POST'])
+def webhook():
+  text = 'i have integrated with flask'
+  values = {'text':text}
+  return json.dump(values)
+
+# run the app
+if __name__ == '__main__':
+    app.run()
