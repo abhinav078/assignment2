0e86f51d2b1b02929db9f885330f1769a0ad3c6e
abhinav078 <43104472+abhinav078@users.noreply.github.com>
2022-10-16 17:08:13 -0400

    Update conversational_ai_assignment.py

--- conversational_ai_assignment.py
+++ conversational_ai_assignment.py
@@ -1,27 +1,26 @@
-import flask
-import os
-from flask import send_from_directory, request
+# import flask dependencies
+from flask import Flask, request
 
-app = flask.Flask(__name__)
+# initialize the flask app
+app = Flask(__name__)
 
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/favicon.png')
-
-@app.route('/')
-@app.route('/home')
-def home():
-    return "Hello World"
-
-@app.route('/webhook', methods=['POST'])
+# create a route for webhook
+@app.route('/webhook', methods=['GET', 'POST'])
 def webhook():
-    req = request.get_json(force=True)
-    print(req)
+    req = request.get_json(silent=True, force=True)
+    fulfillmentText = ''
+    query_result = req.get('queryResult')
+    if query_result.get('action') == 'get.address':
+        ### Perform set of executable code
+        ### if required
+        ### ...
 
+        fulfillmentText = "Hi"
     return {
-        'fulfillmentText': 'Hello from the other side.'
-    }
+            "fulfillmentText": fulfillmentText,
+            "source": "webhookdata"
+        }
 
+# run the app
 if __name__ == '__main__':
-  app.run()
+    app.run()
