d839b5048de89a15a236a39bc88ddeef3cef4858
abhinav078 <43104472+abhinav078@users.noreply.github.com>
2022-10-16 17:26:15 -0400

    Update conversational_ai_assignment.py

--- conversational_ai_assignment.py
+++ conversational_ai_assignment.py
@@ -1,26 +1,5 @@
-# import flask dependencies
-from flask import Flask, request
-
-# initialize the flask app
+from flask import Flask
 app = Flask(__name__)
-
-# create a route for webhook
-@app.route('/webhook', methods=['GET', 'POST'])
-def webhook():
-    req = request.get_json(silent=True, force=True)
-    fulfillmentText = ''
-    query_result = req.get('queryResult')
-    if query_result.get('action') == 'get.address':
-        ### Perform set of executable code
-        ### if required
-        ### ...
-
-        fulfillmentText = "Hi"
-    return {
-            "fulfillmentText": fulfillmentText,
-            "source": "webhookdata"
-        }
-
-# run the app
-if __name__ == '__main__':
-    app.run()
+@app.route('/')
+def hello():
+    return 'Hello, World!'
